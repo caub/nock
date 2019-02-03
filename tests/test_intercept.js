e47348d41142e51b95471c1f4f806fa0f8f5f4a9
nijynot <nijynot@gmail.com>
2019-02-03 12:56:46 +0100

    test(intercept): remove redundant test

--- tests/test_intercept.js
+++ tests/test_intercept.js
@@ -1441,53 +1441,6 @@ test('can use ClientRequest using GET with url', t => {
   req.end()
 })
 
-test('can use ClientRequest using GET with url and options', t => {
-  let dataCalled = false
-
-  const username = 'testuser'
-  const password = 'testpassword'
-  const authString = `${username}:${password}`
-
-  const expectedAuthHeader = `Basic ${Buffer.from(authString).toString(
-    'base64'
-  )}`
-
-  const scope = nock('http://example.test:3414', {
-    reqheaders: {
-      'X-My-Super-Power': /Awesome/i,
-    },
-  })
-    .get('/dsad')
-    .matchHeader('Authorization', val => val === expectedAuthHeader)
-    .reply(202, 'HEHE!')
-
-  const req = new http.ClientRequest('http://example.test/dsad', {
-    method: 'GET',
-    auth: authString,
-    port: 3414,
-    headers: {
-      'X-My-Super-Power': /Awesome/i,
-    },
-  })
-  req.end()
-
-  req.on('response', function(res) {
-    t.equal(res.statusCode, 202)
-    res.on('end', function() {
-      t.ok(dataCalled, 'data event was called')
-      scope.done()
-      t.end()
-    })
-    res.on('data', function(data) {
-      dataCalled = true
-      t.ok(data instanceof Buffer, 'data should be buffer')
-      t.equal(data.toString(), 'HEHE!', 'response should match')
-    })
-  })
-
-  req.end()
-})
-
 test('can use ClientRequest using GET with auth in url and options', t => {
   let dataCalled = false
 
